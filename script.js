76a891bf65880c064ef3c4b034d47d7a40d5a6fa
Kenneth Santiañez <kennethcsantianez@gmail.com>
2025-02-23 13:04:50 +0800

    add music

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
-  }, 1000);
+  }, 100);
 };
+
+let hasPlayed = false; // Flag to track if audio has been played
+
+document.getElementById("playButton").addEventListener("click", function () {
+	console.log('play');
+	if (!hasPlayed) { // Play only if it hasn't played before
+		let audio = new Audio("music.MP3"); // Replace with your actual file
+		audio.play().catch(error => console.log("Autoplay blocked:", error));
+		hasPlayed = true; // Set flag to true after playing
+	}
+});
